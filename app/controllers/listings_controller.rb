d5e3586802e1ed9310d28d061a4327339d4355c1
Karen Ann <kamxlee@gmail.com>
2024-05-30 16:41:08 +0800

    First cut of all listings methods

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,53 @@
 class ListingsController < ApplicationController
+  before_action :set_listing, only: [ :show, :edit, :update, :destroy ]
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+
   def index
     @listings = Listing.all
+  end
+
+  def show
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    @listing.user = current_user
+    if @listing.save
+      # To update path to listing_path(@listing) when show.html.erb view is up
+      redirect_to listings_path, status: :see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @listing.update(listing_params)
+    if @listing.save
+      # To update path to listing_path(@listing) when show.html.erb view is up
+      redirect_to listings_path, status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @listing.destroy
+    redirect_to listings_path, status: :see_other
+  end
+
+  private
+  def listing_params
+    params.require(:listing).permit(:name, :description, :sound_clip, :images, :instruments, :liked_genres, :liked_bands, :looking_for)
+  end
+
+  def set_listing
+    @listing = Listing.find(params[:id])
   end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,8 @@
+MUSICIANS = %w(vocals keyboardist guitarist bassist drummer)
+
 class Listing < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
 
-  validates :looking_for, inclusion: { in: %w(vocals keyboardist guitarist bassist drummer), message: "%{value} is not a valid musician" }
+  validates :looking_for, inclusion: { in: MUSICIANS, message: "%{value} is not a valid musician" }
 end
