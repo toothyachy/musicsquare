6bd9cc91ff9dbe54a4554510085d575c6c676595
Ng Shi Xuan <alexisng.sx@gmail.com>
2024-06-03 15:02:57 +0800

    requests works, doing styling now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :listings
   has_many :requests
+
+  has_many :queues, through: :listings, source: :requests
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
