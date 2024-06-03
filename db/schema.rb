e5e157a702fba9136301c737c46a0df3a436430e
Mistah-Jae <160692244+Mistah-Jae@users.noreply.github.com>
2024-06-03 15:32:25 +0800

    merge with master branch

--- db/schema.rb
+++ db/schema.rb
@@ -14,17 +14,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_01_154836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "availabilities", force: :cascade do |t|
-    t.string "start_time"
-    t.string "end_time"
-    t.bigint "listing_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "date_range"
-    t.string "day"
-    t.index ["listing_id"], name: "index_availabilities_on_listing_id"
-  end
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -51,6 +40,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_01_154836) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "availabilities", force: :cascade do |t|
+    t.string "start_time"
+    t.string "end_time"
+    t.bigint "listing_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "date_range"
+    t.string "day"
+    t.index ["listing_id"], name: "index_availabilities_on_listing_id"
   end
 
   create_table "listings", force: :cascade do |t|
@@ -94,9 +94,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_01_154836) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "availabilities", "listings"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "availabilities", "listings"
   add_foreign_key "listings", "users"
   add_foreign_key "requests", "listings"
   add_foreign_key "requests", "users"
